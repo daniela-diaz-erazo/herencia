0b9b1271e55f96b58c1157f57dc91936af591e52
Daniela Diaz <ampa.dde2106@gmail.com>
2020-12-10 11:10:05 -0500

    Creacion de clase Persona, interfaz ServicioPersona, y la implementacion de ServicioPersona

---
+++ src/main/java/com/clearminds/impl/ServicioPersona.java
@@ -0,0 +1,22 @@
+package com.clearminds.impl;
+
+import com.clearminds.model.Persona;
+
+public class ServicioPersona implements com.clearminds.interfaces.ServicioPersona {
+
+	public boolean insertar(Persona persona) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean actualizar(Persona persona) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean eliminar(Persona persona) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
